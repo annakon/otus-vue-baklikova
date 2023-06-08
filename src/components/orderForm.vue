997188647328c27885435e0468fce2de8c3fd7d4
denis <denis.baklykov@boxberry.ru>
2023-06-08 14:52:06 +0300

    начало работы с валидацией и доработка формы заказа

--- src/components/orderForm.vue
+++ src/components/orderForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="row g-3">
+  <form class="row g-3" @submit.prevent="onSubmit">
     <div class="col-md-8">
       <label for="nameGoods">ФИО</label>
       <input type="text" class="form-control" id="nameGoods" v-model="nameNew" />
@@ -12,6 +12,13 @@
       <label for="address">Aдрес</label>
       <input type="text" class="form-control" id="address" v-model="address" />
     </div>
+    <div class="col-12 my-1">
+      <div class="form-check mb-2 mr-sm-2">
+        <input class="form-check-input" type="checkbox" id="inlineFormCheck" />
+        <label class="form-check-label" for="inlineFormCheck"> согласен с правилами обработки заказа </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Оформить</button>
+    </div>
   </form>
 </template>
 
@@ -21,6 +28,10 @@ import { ref } from 'vue';
 const nameNew = ref('');
 const email = ref('');
 const address = ref('');
+
+function onSubmit() {
+  console.log('Submitted');
+}
 </script>
 
 <style scoped>
