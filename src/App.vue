8eef6d77621844da7c0eb2775657388b9ffc9f99
denis <denis.baklykov@boxberry.ru>
2023-06-15 17:58:05 +0300

    формат

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,5 @@
 <script setup>
-import { computed, reactive, ref } from 'vue';
-import { goods } from './api';
 import SearchForm from '@/components/searchForm.vue';
-
-const isAdd = ref(false);
-const isOrder = ref(false);
-const isFind = ref(false);
-const forSearch = reactive([]);
-const goodsList = computed(goodsToShow);
-function goodsToShow() {
-  return isFind.value
-    ? goods.value.filter(
-        (t) =>
-          (t?.price === forSearch.value.price || typeof forSearch.value.price === 'undefined') &&
-          t?.title.indexOf(forSearch.value.name) !== -1
-      )
-    : goods.value;
-}
-
-function setVisible(find, add, order) {
-  isAdd.value = add;
-  isFind.value = find;
-  isOrder.value = order;
-}
-function findCard(findObj) {
-  forSearch.value = findObj;
-  setVisible(true, false, false);
-}
 </script>
 
 <template>
@@ -37,7 +10,7 @@ function findCard(findObj) {
         <router-link to="/add" class="navbar-brand">Добавить товар</router-link>
         <router-link to="/order" class="navbar-brand">Оформить заказ</router-link>
         <router-link to="/" class="navbar-brand">Просмотр списка</router-link>
-        <search-form @find-card="findCard"></search-form>
+        <search-form></search-form>
       </div>
     </nav>
   </header>
